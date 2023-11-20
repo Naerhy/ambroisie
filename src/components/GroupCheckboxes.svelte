b68bb88088e7ad8481db477d1d6747ef58be9546
Naerhy <naerhy@protonmail.com>
2023-11-20 19:35:47 +0100

    Implement GroupCheckboxes component

---
+++ src/components/GroupCheckboxes.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let title: string;
+	export let name: string;
+	export let values: string[];
+	export let groupValues: any[];
+</script>
+
+<div class="title">{title}</div>
+{#each values as value}
+{@const id = `gc-${name}-${value}`}
+<div>
+	<input type="checkbox" {id} {value} bind:group={groupValues}/>
+	<label for={id}>{value.charAt(0).toUpperCase() + value.slice(1)}</label>
+</div>
+{/each}
+
+<style>
+	.title {
+		font-weight: 700;
+	}
+</style>
--- src/components/RecipesList.svelte
+++ src/components/RecipesList.svelte
@@ -2,15 +2,16 @@
 	import type { RecipeFN } from "../types";
 	import { createEventDispatcher } from "svelte";
 	import RecipeCard from "./RecipeCard.svelte";
+	import GroupCheckboxes from "./GroupCheckboxes.svelte";
 
 	export let recipes: RecipeFN[];
 
 	const dispatch = createEventDispatcher();
 
 	let filter = "";
-	let mealTypes: string[] = ["breakfast", "lunch", "tea", "dinner"];
-	let difficulties: string[] = ["easy", "medium", "hard"];
-	let cookingTimes: string[] = ["short", "medium", "long"];
+	let mealTypes = ["breakfast", "lunch", "tea", "dinner"];
+	let difficulties = ["easy", "medium", "hard"];
+	let cookingTimes = ["short", "medium", "long"];
 	let vegetarian = false;
 
 	function clickRecipe(recipe: RecipeFN): void {
@@ -23,60 +24,33 @@
 <aside>
 	<input type="text" placeholder="Filter by name..." bind:value={filter} />
 	<div>
-		<h4>Meal type</h4>
-		{#each ["breakfast", "lunch", "tea", "dinner"] as mealType}
-		<div class="checkbox">
-			<input
-				type="checkbox"
-				id={`meal-type-${mealType}`}
-				name="meal-type"
-				value={mealType}
-				bind:group={mealTypes}
-			/>
-			<label for={`meal-type-${mealType}`}>
-				{mealType.charAt(0).toUpperCase() + mealType.slice(1)}
-			</label>
-		</div>
-		{/each}
+		<GroupCheckboxes
+			title="Meal type"
+			name="meal-type"
+			values={["breakfast", "lunch", "tea", "dinner"]}
+			bind:groupValues={mealTypes}
+		/>
 	</div>
 	<div>
-		<h4>Difficulty</h4>
-		{#each ["easy", "medium", "hard"] as difficulty}
-		<div class="checkbox">
-			<input
-				type="checkbox"
-				id={`difficulty-${difficulty}`}
-				name="difficulty"
-				value={difficulty}
-				bind:group={difficulties}
-			/>
-			<label for={`difficulty-${difficulty}`}>
-				{difficulty.charAt(0).toUpperCase() + difficulty.slice(1)}
-			</label>
-		</div>
-		{/each}
+		<GroupCheckboxes
+			title="Difficulty"
+			name="difficulty"
+			values={["easy", "medium", "hard"]}
+			bind:groupValues={difficulties}
+		/>
 	</div>
 	<div>
-		<h4>Cooking time</h4>
-		{#each ["short", "medium", "long"] as cookingTime}
-		<div class="checkbox">
-			<input
-				type="checkbox"
-				id={`cooking-time-${cookingTime}`}
-				name="cooking-time"
-				value={cookingTime}
-				bind:group={cookingTimes}
-			/>
-			<label for={`cooking-time-${cookingTime}`}>
-				{cookingTime.charAt(0).toUpperCase() + cookingTime.slice(1)}
-			</label>
-		</div>
-		{/each}
+		<GroupCheckboxes
+			title="Cooking time"
+			name="cooking-time"
+			values={["short", "medium", "long"]}
+			bind:groupValues={cookingTimes}
+		/>
 	</div>
 	<div>
-		<h4>Dietary practice</h4>
-		<div class="checkbox">
-			<input type="checkbox" id="vegetarian" name="vegetarian" bind:checked={vegetarian} />
+		<div class="title">Dietary practice</div>
+		<div>
+			<input type="checkbox" id="vegetarian" bind:checked={vegetarian} />
 			<label for="vegetarian">Vegetarianism</label>
 		</div>
 	</div>
@@ -105,15 +79,6 @@
 		row-gap: 1rem;
 	}
 
-	aside h4 {
-		font-size: 1.125rem;
-		line-height: 1.75rem;
-	}
-
-	.checkbox {
-		margin-left: 0.5rem;
-	}
-
 	input[type=text] {
 		background-color: var(--background-dark);
 		border: 1px solid var(--stroke);
@@ -127,6 +92,10 @@
 
 	input[type=text]:focus {
 		outline: 2px solid var(--highlight);
+	}
+
+	.title {
+		font-weight: 700;
 	}
 
 	section {
