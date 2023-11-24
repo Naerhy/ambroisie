08f755c209a39e3dc371e3124a18c09c435efd7d
Naerhy <naerhy@protonmail.com>
2023-11-24 21:58:07 +0100

    Set up config service to load env varibles from .env (backend)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from "@nestjs/common";
 import { RecipesModule } from "./recipes/recipes.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Recipe } from "./recipes/recipe.entity";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				BACKEND_PORT: Joi.number().required()
+			}),
+			isGlobal: true
+		}),
 		TypeOrmModule.forRoot({
 			type: "sqlite",
 			database: "db.sqlite",
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-	await app.listen(3000);
+	await app.listen(app.get(ConfigService).get<number>("BACKEND_PORT")!);
 }
 
 bootstrap();
