b424b50aaec3d85586c731dada11b9cd634f867d
Naerhy <naerhy@protonmail.com>
2023-11-28 20:06:40 +0100

    Fix max size of body requests (backend)
    Prior to this commit, Nestjs would reject any requests bigger than 100kb.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { json } from "express";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.use(json({ limit: "2mb" }));
 	// TODO: limit CORS header to specific origins only
 	app.enableCors();
 	await app.listen(app.get(ConfigService).get<number>("BACKEND_PORT")!);
