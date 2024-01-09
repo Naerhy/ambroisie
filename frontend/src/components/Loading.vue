d72373b050856e85605c9f51954bb728397901a7
Naerhy <naerhy@protonmail.com>
2024-01-09 16:48:27 +0100

    Add spinner on Loading view (frontend)

--- frontend/src/components/Loading.vue
+++ frontend/src/components/Loading.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+	import Loader from "./Loader.vue";
+</script>
+
 <template>
 	<main>
-		<p>Loading...</p>
+		<Loader />
+		<div>Website is loading, please wait.</div>
 	</main>
 </template>
+
+<style scoped>
+	main {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		row-gap: 6rem;
+	}
+
+	div {
+		font-size: 1.125rem;
+		line-height: 1.75rem;
+	}
+</style>
