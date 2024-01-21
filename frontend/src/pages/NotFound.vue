f156570220298572bcd61cf0ef0846126c565f8b
Naerhy <naerhy@protonmail.com>
2024-01-21 18:14:31 +0100

    Improve design of 404 page (frontend)

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -1,4 +1,38 @@
 <template>
-	<p>NOT FOUND</p>
-	<router-link to="/">Retourner à l'accueil</router-link>
+	<main>
+		<section>
+			<img src="/404.svg" width="500px" alt="Illustration erreur 404" />
+			<h1>Page introuvable !</h1>
+			<p>Assurez-vous d'avoir entré la bonne adresse web.</p>
+			<router-link to="/">Retourner à l'accueil</router-link>
+		</section>
+	</main>
 </template>
+
+<style scoped>
+	main {
+		align-items: center;
+		display: flex;
+		justify-content: center;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 2.25rem;
+		line-height: 2.5rem;
+	}
+
+	a {
+		color: var(--highlight);
+		margin-top: 1rem;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
