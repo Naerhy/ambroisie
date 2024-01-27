df32e33003efaa5e632dd384ab79d9c582743ac0
Naerhy <naerhy@protonmail.com>
2024-01-28 00:43:01 +0100

    Improve look of Home page (frontend)

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -4,8 +4,8 @@
 			<h2>En manque d'idées de repas ?</h2>
 			<img src="/eating.svg" alt="2 people eating together" />
 			<p>
-				Ambroisie est né de l'envie de vouloir faciliter la planification des repas de la semaine.<br />
-				Ainsi, vous trouverez tous types de repas parmi le catalogue disponible, allant des repas simples voire déjà préparés à des repas plus complexes avec recettes.<br />
+				Ambroisie est né de l'envie de vouloir faciliter la planification des repas de la semaine.
+				Ainsi, vous trouverez tous types de repas parmi le catalogue disponible, allant des repas simples voire déjà préparés à des repas plus complexes avec recettes.
 				Plusieurs filtres sont disponibles afin d'affiner vos recherches selon vos préférences.
 			</p>
 			<router-link to="/repas" class="link">Accéder à la liste des repas</router-link>
@@ -18,19 +18,19 @@
 		align-items: center;
 		display: flex;
 		flex-direction: column;
+		margin: auto;
+		max-width: 800px;
+		padding: 0 1.5rem;
 		row-gap: 1rem;
 	}
 
 	h2 {
 		font-size: 2.25rem;
 		line-height: 2.5rem;
+		text-align: center;
 	}
 
 	img {
-		width: 500px;
-	}
-
-	p {
-		width: 50%;
+		width: 600px;
 	}
 </style>
